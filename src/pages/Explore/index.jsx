e9cfb74febccfaa37706912dfa46e2fb483ec1bb
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-12 17:39:28 +0300

    added empty pages

---
+++ src/pages/Explore/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from '../../components/UI/SearchBar'
+
+const Explore = () => {
+  return (
+    <div className='flex flex-col items-center'>
+      <SearchBar />
+    </div>
+  )
+}
+
+export default Explore
---
+++ src/pages/Planner/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Planner = () => {
+  return (
+    <div>Planner</div>
+  )
+}
+
+export default Planner
