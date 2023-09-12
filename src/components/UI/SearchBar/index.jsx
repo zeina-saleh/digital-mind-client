538466fa4c1770ef38da686d54a18de409f688ea
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-12 20:45:29 +0300

    added searchBar component

--- src/components/UI/SearchBar/index.jsx
+++ src/components/UI/SearchBar/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = () => {
   return (
-    <div className='search-bar flex rounded-l-full rounded-r-full border-[#1e1e1e]'>
-        fjdhgnhmjh,
+    <div className='search-bar flex rounded-l-full rounded-r-full w-6/12 h-12 p-1 px-2.5 gap-3 items-center'>
+      <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#1e1e1e",}} className='w-6 h-8' /> 
+      <input type='text' className='search-input w-9/12 ' placeholder='search ideas'></input>
+      <div></div>
     </div>
   )
 }
