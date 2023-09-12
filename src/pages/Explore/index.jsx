c9a4afdf3f98bb4ca2ff89ecd9a35e209a792f56
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-12 21:04:57 +0300

    implemented fetch ideas logic

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import './style.css'
 import SearchBar from '../../components/UI/SearchBar'
+import { useEffect, useState } from 'react'
+import { sendRequest } from '../../config/request'
+import { Link } from 'react-router-dom'
+import IdeaCard from '../../components/UI/IdeaCard'
 
 const Explore = () => {
+
+  const [ideas, setIdeas] = useState([]);
+
+  const fetchIdeas = async () => {
+    try {
+      const response = await sendRequest({ route: "/getIdeas", body: "" });
+      setIdeas(response);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchIdeas();
+  }, []);
+
   return (
-    <div className='flex flex-col items-center'>
+    <div className='flex flex-col items-center gap-5'>
       <SearchBar />
+      <h6>Explore ideas and find people with similar interests</h6>
+      <div className='flex pl-5 w-10/12 h-screen gap-5 flex-wrap'>
+        {ideas.map(idea => (
+          <Link key={idea.id} to={`/home/planner`}>
+            <IdeaCard idea={idea} />
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
