78dd759cc59bf2554f9e11f297dcfa4504623c8e
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-12 17:34:56 +0300

    implemented login and register logic

--- src/components/base/LoginForm/index.jsx
+++ src/components/base/LoginForm/index.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import { useState } from 'react'
+import { sendRequest } from '../../../config/request';
+import { useNavigate } from "react-router-dom";
 import Input from '../../UI/Input'
 import './style.css'
 import Button from '../../UI/Button'
 
-const LoginForm = (event) => {
-  const print = () => {
-    console.log(event.target.value)
+const LoginForm = ({ onToggle }) => {
+
+  const navigation = useNavigate();
+  //
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [error, setError] = useState(null);
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const loginHandler = async () => {
+
+    if (!validateEmail(credentials.email)) {
+      setError("Invalid Email Address");
+      return;
+    }
+
+    if (credentials.password.length < 8) {
+      setError("Password must be at least 8 characters long");
+      return;
+    }
+
+    try {
+      const response = await sendRequest({ method: "POST", route: "/login", body: credentials });
+      localStorage.setItem("access_token", response.token);
+      navigation("/home");
+    } catch (error) {
+      console.log(error);
+      setError("Incorrect Email/Password");
+    }
   }
   return (
     <div className='flex flex-col items-start gap-5 w-80'>
@@ -13,11 +45,11 @@ const LoginForm = (event) => {
         <h1 className='title'>Login</h1>
         <h6 className='font-semibold'>Enter your account details</h6>
       </div>
-      <Input label={'Email'} onChange={print} />
-      <Input label={'Password'} onChange={print} type='password' />
+      <Input label={'Email'} onChange={(email) => setCredentials({ ...credentials, email })} />
+      <Input label={'Password'} onChange={(password) => setCredentials({ ...credentials, password })} type='password' />
       <h6>Forgot password?</h6>
-      <h6 className='flex w-full text-[#20E399] cursor-pointer'>Don't have an account? Register</h6>
-      <Button text={"Login"} onClick={print} classname={'w-full'} />
+      <h6 className='flex w-full text-[#20E399] cursor-pointer' onClick={onToggle}>Don't have an account? Register</h6>
+      <Button text={"Login"} onClick={loginHandler} classname={'w-full text-white'} />
     </div>
   )
 }
--- src/components/base/RegisterForm/index.jsx
+++ src/components/base/RegisterForm/index.jsx
@@ -1,24 +1,41 @@
 import React from 'react'
+import { useState } from 'react'
+import { sendRequest } from '../../../config/request';
+import { useNavigate } from "react-router-dom";
 import Input from '../../UI/Input'
 import './style.css'
 import Button from '../../UI/Button'
 
-const RegisterForm = () => {
-  const print = () => {
-    console.log(event.target.value)
-  }
+const RegisterForm = ({ onToggle }) => {
+
+  const navigation = useNavigate();
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+    name: ""
+  });
+
+  const registerHandler = async () => {
+    try {
+      const response = await sendRequest({method: "POST", route: "/register", body: credentials});
+      localStorage.setItem("access_token", response.token);
+      navigation("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className='flex flex-col items-start gap-5 w-80'>
       <div className='flex flex-col gap-2'>
         <h1 className='title'>Signup</h1>
         <h6 className='font-semibold'>Enter your account details</h6>
       </div>
-      <Input label={'Name'} onChange={print} />
-      <Input label={'Email'} onChange={print} />
-      <Input label={'Password'} onChange={print} type='password' />
+      <Input label={'Name'} value={credentials.name} onChange={(name) => setCredentials({ ...credentials, name })} />
+      <Input label={'Email'} value={credentials.email} onChange={(email) => setCredentials({ ...credentials, email })} />
+      <Input label={'Password'} value={credentials.password} onChange={(password) => setCredentials({ ...credentials, password })} type='password' />
       <h6>Forgot password?</h6>
-      <h6 className='flex w-full text-[#20E399] cursor-pointer'>Don't have an account? Register</h6>
-      <Button text={"Login"} onClick={print} classname={'w-full'} />
+      <h6 className='flex w-full text-[#20E399] cursor-pointer' onClick={onToggle}>Already have an account? Login</h6>
+      <Button text={"Signup"} onClick={registerHandler} classname={'w-full text-white'} />
     </div>
   )
 }
