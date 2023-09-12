4bdcec98269a51853b8929ee6473b42e5c4b8918
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-12 17:37:44 +0300

    modified button component

--- src/components/UI/Button/index.jsx
+++ src/components/UI/Button/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 const Button = ({ text, onClick, classname, enabled = true }) => {
-    const clickHandler = () => {
+    const clickHandler = (e) => {
+        e.preventDefault()
         if (enabled) {
             onClick();
         }
     };
     return (
         <button className={`${classname} bg-[#20E399] w-[100px] h-10 rounded-l-full rounded-r-full font-medium`}
-            onClick={() => clickHandler()}>{text}</button>
+            onClick={(e) => clickHandler(e)}>{text}</button>
     )
 }
 
