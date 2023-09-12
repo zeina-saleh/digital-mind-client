96590f07a45fe0838e9a9510edc15c1a9c488d87
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-12 17:37:59 +0300

    modified input component

--- src/components/UI/Input/index.jsx
+++ src/components/UI/Input/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './style.css'
 
-const Input = ({onChange, label, placeholder = "", type = "text" }) => {
+const Input = ({onChange, label, placeholder = "", type = "text"}) => {
   return (
     <div className='flex flex-col items-start'>
         <div className='input-wrapper flex flex-col items-start gap-1 w-full'>
-        <label htmlFor="input">{label}</label>
-            <input type={type} id='input' placeholder={placeholder} className='text-input p-1 w-80'/></div>
+        <label htmlFor={label.toLowerCase()}>{label}</label>
+            <input type={type} name={label.toLowerCase()} id={label.toLowerCase()} placeholder={placeholder} className='text-input p-1 w-80' onChange={(e) => onChange(e.target.value)}/></div>
     </div>
   )
 }
