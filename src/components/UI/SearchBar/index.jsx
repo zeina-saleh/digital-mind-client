90bee960e55f855a71a34998364e55293ee3d0bf
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-12 17:39:42 +0300

    added new components

---
+++ src/components/UI/SearchBar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const SearchBar = () => {
+  return (
+    <div className='search-bar flex rounded-l-full rounded-r-full border-[#1e1e1e]'>
+        fjdhgnhmjh,
+    </div>
+  )
+}
+
+export default SearchBar
---
+++ src/components/base/Navbar0/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+
+const Navbar0 = () => {
+    return (
+        <>
+            <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+                
+            </div>
+            <Outlet />
+        </>
+    )
+}
+
+export default Navbar0
