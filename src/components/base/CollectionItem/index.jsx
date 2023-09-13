8e4e7256273607fbdafa18bf7f7298ac154cf0ec
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-13 23:03:21 +0300

    added delete idea icon

--- src/components/base/CollectionItem/index.jsx
+++ src/components/base/CollectionItem/index.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faChevronUp, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronUp, faSquarePlus, faSquareMinus } from '@fortawesome/free-solid-svg-icons';
 import './style.css'
 import logo from '../../../assets/logo.svg'
-import Button2 from '../../UI/Button2';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
-const CollectionItem = ({ collection, setIsAddIdeaClicked, handleOpenModal, setCollectionId}) => {
+const CollectionItem = ({ collection, setIsAddIdeaClicked, handleOpenModal, setCollectionId, handleOpenConsentModal, setIdeaId }) => {
 
     const [showIdeas, setShowIdeas] = useState(false)
     const [chevron, setChevron] = useState(faChevronDown)
@@ -24,6 +22,11 @@ const CollectionItem = ({ collection, setIsAddIdeaClicked, handleOpenModal, setC
         handleOpenModal()
     }
 
+    const handleDeleteIdea = (ideaId) => {
+        setIdeaId(ideaId);
+        handleOpenConsentModal()
+    }
+
     return (
         <div className='flex flex-col'>
             <div className='collection-item flex items-center justify-between p-2'>
@@ -31,20 +34,17 @@ const CollectionItem = ({ collection, setIsAddIdeaClicked, handleOpenModal, setC
                 <FontAwesomeIcon icon={chevron} style={{ color: "#1e1e1e", }} className='cursor-pointer' onClick={onToggle} />
             </div>
             {(collection.ideas.length !== 0 && showIdeas) ? (
-                <div className='flex px-2 py-4 gap-5 mt-4'>
+                <div className='flex flex-wrap px-10 py-4 gap-5 mt-4'>
                     {collection.ideas.map(idea => (
-                        <Link key={idea.id} to={`/home/planner`}>
-                            <div className='flex flex-col'>
-                                <div className='idea-item flex items-center justify-center w-64 h-60'>
-                                    <img src={logo} alt="" className='w-40 h-40' />
-                                </div>
-                                <div className='flex flex-col w-full p-2'>
-                                    <p className='font-semibold'>{idea.title}</p>
-                                </div>
+                        <div key={idea.id} className='flex flex-col'>
+                            <div className='idea-item flex flex-col items-center w-64 h-60 px-2 pt-2'>
+                                <FontAwesomeIcon icon={faSquareMinus} style={{ color: "#20e399", }} onClick={() => handleDeleteIdea(idea.id)} className='self-end cursor-pointer h-7 w-7' />
+                                <img src={logo} alt="" className='w-40 h-40' />
                             </div>
-                        </Link>
+                            <Link to={`/home/planner`}><div className='w-fit p-2 font-normal hover:text-[#20e399]'>{idea.title}</div></Link>
+                        </div>
                     ))}
-                    <FontAwesomeIcon icon={faSquarePlus} style={{ color: "#20e399", }} className='add-idea h-7 w-7' onClick={handleAddIdea} />
+                    <FontAwesomeIcon icon={faSquarePlus} style={{ color: "#20e399", }} className='add-idea h-7 w-7 cursor-pointer' onClick={handleAddIdea} />
 
                 </div>
             ) : (
