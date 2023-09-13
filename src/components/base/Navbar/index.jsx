293b1b08d395391fc0595f387fea8ffa6058f195
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-13 15:11:51 +0300

    implemented logout function in navbar

--- src/components/base/Navbar/index.jsx
+++ src/components/base/Navbar/index.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react';
 import logo from '../../../assets/logo.svg'
 import Button from '../../UI/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faXmark ,faBars} from '@fortawesome/free-solid-svg-icons';
+import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
+import { sendRequest } from '../../../config/request';
 import './style.css'
 
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
+    const navigation = useNavigate();
+
 
     const handleNav = () => {
         setNav(!nav);
     }
 
-    const logout = () => {
-        console.log("logout")
+    const logout = async () => {
+        try {
+            const response = await sendRequest({ route: "/logout", body: "" });
+            console.log(response);
+            navigation('/landing')
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -30,11 +39,11 @@ const Navbar = () => {
                 </div>
                 {!nav ? <>
                     <div className='flex items-center'>
-                        <Button text={"Logout"} onClick={logout} classname={'nav-link text-xl w-24 h-10'}/>
+                        <Button text={"Logout"} onClick={logout} classname={'nav-link text-xl w-24 h-10'} />
                     </div>
                 </>
                     : <>
-                        <FontAwesomeIcon icon={faBars} style={{color: "#1e1e1e",}} className="w-14 h-6 cursor-pointer"/> 
+                        <FontAwesomeIcon icon={faBars} style={{ color: "#1e1e1e", }} className="w-14 h-6 cursor-pointer" />
                         {/* <ul className='hidden md:flex space-x-4'>
                             <li className='p-4 cursor-pointer'>Explore</li>
                             <li className='p-4 cursor-pointer'>Planner</li>
