8be57e45b4a52658969b4c1c2fad6a23f0dcedb5
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-14 15:41:35 +0300

    changed button2 styling

--- src/components/UI/Button2/index.jsx
+++ src/components/UI/Button2/index.jsx
@@ -10,10 +10,10 @@ const Button2 = ({ text, onClick, classname, enabled = true, icon }) => {
         }
     };
     return (
-        <div className={`${classname} flex h-9 p-1 px-3 items-center justify-center rounded-l-full rounded-r-full font-medium cursor-pointer button2`}
+        <div className={`${classname} flex h-9 p-1 px-3 items-center justify-center rounded-l-full rounded-r-full font-medium cursor-pointer  hover:bg-white button2`}
             onClick={(e) => clickHandler(e)}>
             <div className='flex items-center justify-evenly gap-2'>
-            <FontAwesomeIcon icon={icon} style={{ color: "#20E399", }} />
+            <FontAwesomeIcon icon={icon} className='text-[#1ED690] '/>
                 <p>{text}</p>
             </div>
         </div >
