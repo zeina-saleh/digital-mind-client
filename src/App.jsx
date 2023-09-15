528f1f54ca5d1d16f950a036a6c90f40cbba6ec2
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-15 18:42:35 +0300

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/App.css';
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route, Outlet, useNavigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar0 from './components/base/Navbar0';
 import Navbar from './components/base/Navbar';
 import Landing from './pages/Landing';
@@ -10,6 +11,15 @@ import Collections from './pages/Collections';
 import Map from './pages/Map';
 
 function App() {
+
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (!token) navigate('/login')
+  }, [location.pathname]);
+
   return (
     <div className='App'>
       <Routes>
@@ -19,7 +29,7 @@ function App() {
           <Route index element={<Explore />} />
           <Route path='/home/planner' element={<Planner />} />
           <Route path='/home/collections' element={<Collections />} />
-          <Route path='/home/collections/idea/:ideaId' element={<Map />} />
+          <Route path='/home/collections/idea/:ideaId?' element={<Map />} />
         </Route>
       </Routes>
     </div>
