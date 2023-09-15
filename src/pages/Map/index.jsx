4ececd469fa265d6e7823d8aeb73cd8c267817b0
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-15 17:17:36 +0300

    implemented map page layout

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { sendRequest } from '../../config/request'
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faComments } from '@fortawesome/free-regular-svg-icons';
 import Button2 from '../../components/UI/Button2';
@@ -6,22 +9,40 @@ import './style.css'
 
 const Map = () => {
 
+    const { ideaId } = useParams();
+    const [idea, setIdea] = useState([]);
+
+    const fetchIdea = async () => {
+        try {
+            const response = await sendRequest({ route: `/getIdeas/${ideaId}`, body: "" });
+            setIdea(response.idea);
+            console.log(idea)
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchIdea();
+    }, []);
+
     const print = () => {
-        console.log('clicked')
+        console.log('print')
     }
+
     return (
-        <div className='flex flex-col gap-4'>
-            <div className='line w-10/12 self-center'></div>
-            <div className='flex flex-col items-center gap-5'>
-                <div className='flex justify-between w-10/12'>
-                    <div></div>
-                    <div className='flex gap-1'>
-                        <Button2 text={"Group Discussion"} onClick={print} icon={faComments} />
-                        <Button2 text={"Add Resource"} onClick={print} icon={faPlus} />
-                    </div>
+        <>
+            <div className='flex flex-col items-center w-full gap-4'>
+                <div className='line w-11/12'></div>
+                <div className='flex gap-1 w-11/12 justify-end'>
+                    <Button2 text={"Group Discussion"} onClick={print} icon={faComments} />
+                    <Button2 text={"Add Resource"} onClick={print} icon={faPlus} />
                 </div>
             </div>
-        </div>
+            <div className='flex flex-col justify-center items-center w-full min-h-screen'>
+                <div className='idea-title text-3xl font-medium'>{idea.title}</div>
+            </div>
+        </>
     )
 }
 
