5e9e78c19cc4828f6a8e6963b4e4df83cbf88440
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-16 10:51:19 +0300

    created modal form component

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -5,12 +5,18 @@ import { sendRequest } from '../../config/request'
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faComments } from '@fortawesome/free-regular-svg-icons';
 import Button2 from '../../components/UI/Button2';
+import Modal from 'react-modal';
+import ModalForm from '../../components/base/ModalForm';
 import './style.css'
 
 const Map = () => {
 
     const { ideaId } = useParams();
     const [idea, setIdea] = useState([]);
+    const [openModal, setOpenModal] = useState(false)
+
+    const handleOpenModal = () => setOpenModal(true)
+    const handleCloseModal = () => setOpenModal(false)
 
     const fetchIdea = async () => {
         try {
@@ -36,12 +42,15 @@ const Map = () => {
                 <div className='line w-11/12'></div>
                 <div className='flex gap-1 w-11/12 justify-end'>
                     <Button2 text={"Group Discussion"} onClick={print} icon={faComments} />
-                    <Button2 text={"Add Resource"} onClick={print} icon={faPlus} />
+                    <Button2 text={"Add Resource"} onClick={handleOpenModal} icon={faPlus} />
                 </div>
             </div>
             <div className='flex flex-col justify-center items-center w-full min-h-screen'>
-                <div className='idea-title text-3xl font-medium'>{idea.title}</div>
+                <div className='idea-title text-2xl font-medium'>{idea.title}</div>
             </div>
+            <Modal overlayClassName='overlay' isOpen={openModal} onRequestClose={handleCloseModal} className={'modal w-1/3 h-fit flex flex-col gap-5 py-8 px-9 active:border-0 bg-white'}>
+                <ModalForm handleCloseModal={handleCloseModal} ideaId={ideaId}/>
+            </Modal>
         </>
     )
 }
