d5c4e3de59ae8a5543bab61336d744bc84e84e63
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-16 17:43:42 +0300

    created Navbar0 component

--- src/components/base/Navbar0/index.jsx
+++ src/components/base/Navbar0/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
-
+import { Outlet, Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import logo from '../../../assets/logo.svg'
+import Button from '../../UI/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Navbar0 = () => {
+
+    const navigation = useNavigate();
+
+    const login = async () => {
+            navigation('/login')
+    }
     return (
         <>
-            <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
-                
+            <div className='flex justify-between items-center h-24 w-full mx-auto px-4 mb-5'>
+                <img src={logo} alt="logo" className="w-20 h-12" />
+                <div className='hidden md:flex space-x-4 text-3xl'>
+                    {/* <Link to='/home' className='nav-link p-4 cursor-pointer '>Explore</Link>
+                    <Link to='/home/planner' className='nav-link p-4 cursor-pointer'>Planner</Link>
+                    <Link to='/home/collections' className='nav-link p-4 cursor-pointer'>Collections</Link> */}
+                </div>
+                <div className='flex items-center'>
+                    <Button text={"Login"} onClick={login} classname={'nav-link text-xl w-24 h-10'} />
+                </div>
             </div>
-            <Outlet />
         </>
     )
 }
