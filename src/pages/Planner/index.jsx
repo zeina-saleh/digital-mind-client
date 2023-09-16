768a246f874faf958544645e760d00311e614309
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-16 16:04:21 +0300

    implemented planner section template

--- src/pages/Planner/index.jsx
+++ src/pages/Planner/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import './style.css'
 
 const Planner = () => {
   return (
-    <div>Planner</div>
+    <div className='flex justify-center w-full h-screen'>
+      <div className='w-10/12'>
+        <FullCalendar
+          plugins={[timeGridPlugin]}
+          initialView="timeGrid"
+          weekends={true}
+          events={''}
+          eventBackgroundColor='#78FFCC'
+        />
+      </div>
+    </div>
   )
 }
 
