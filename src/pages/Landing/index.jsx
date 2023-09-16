0b28ba25fb11e151821cfb92657de15b88646928
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-16 19:52:31 +0300

    implemented landing page

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import constellation from '../../assets/constellation.svg'
+import './style.css'
+import Navbar0 from '../../components/base/Navbar0'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='flex flex-col justify-center items-center w-full h-screen'>
+      <div className='idea-title self-center text-8xl text-center w-full h-full'>
+        <div className='flex flex-col w-full h-full' style={{ backgroundImage: `url(${constellation})` }}>
+          <Navbar0 />
+          <section className='flex flex-col w-full h-3/5 justify-center' >
+            <div className='idea-title text-8xl font-medium'>Digital Mind</div>
+          </section>
+        </div>
+        <section className='flex flex-col w-full h-full justify-center'>
+          <div className='idea-title text-7xl font-medium'>Digital Mind</div>
+        </section>
+      </div>
+    </div>
   )
 }
 
