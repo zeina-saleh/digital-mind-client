b243c333978455d497cca38e88da1f5015cb9215
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-17 14:01:54 +0300

    rendered resources map

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -7,13 +7,16 @@ import { faComments } from '@fortawesome/free-regular-svg-icons';
 import Button2 from '../../components/UI/Button2';
 import Modal from 'react-modal';
 import ModalForm from '../../components/base/ModalForm';
+import TreeNode from '../../components/base/TreeNode';
 import './style.css'
 
 const Map = () => {
 
     const { ideaId } = useParams();
-    const [idea, setIdea] = useState([]);
     const [openModal, setOpenModal] = useState(false)
+    const [idea, setIdea ] = useState({})
+    const [elements, setElements] = useState([])
+    const [isUploaded, setIsUploaded] = useState(false)
 
     const handleOpenModal = () => setOpenModal(true)
     const handleCloseModal = () => setOpenModal(false)
@@ -21,8 +24,8 @@ const Map = () => {
     const fetchIdea = async () => {
         try {
             const response = await sendRequest({ route: `/getIdeas/${ideaId}`, body: "" });
-            setIdea(response.idea);
-            console.log(idea)
+            setIdea(response)
+            setElements([...response.text_res, ...response.file_res])
         } catch (error) {
             console.log(error);
         }
@@ -30,11 +33,17 @@ const Map = () => {
 
     useEffect(() => {
         fetchIdea();
-    }, []);
+    }, [isUploaded]);
 
     const print = () => {
         console.log('print')
     }
+
+    const width = 400;
+    const height = 200;
+    const R = 90;
+    const x0 = width / 2;
+    const y0 = height / 2;
 
     return (
         <>
@@ -46,10 +55,21 @@ const Map = () => {
                 </div>
             </div>
             <div className='flex flex-col justify-center items-center w-full min-h-screen'>
-                <div className='idea-title text-2xl font-medium'>{idea.title}</div>
+                {/* <div className='idea-title text-2xl font-medium'>{idea.title}</div> */}
+                <svg viewBox='0 0 600 400'>
+                <TreeNode
+                    x={x0} y={y0} caption={idea.title} level={0} />
+                {elements.map((element, index) => (
+                    <TreeNode key={index}
+                        x={x0 + R * Math.cos(index * Math.PI * 2 / (elements.length + 1))}
+                        y={y0 + R * Math.sin(index * Math.PI * 2 / (elements.length + 1))}
+                        caption={element.caption!==null? element.caption : element.text} level={1} />
+                ))}
+            </svg>
+                
             </div>
             <Modal overlayClassName='overlay' isOpen={openModal} onRequestClose={handleCloseModal} className={'modal w-1/3 h-fit flex flex-col gap-5 py-8 px-9 active:border-0 bg-white'}>
-                <ModalForm handleCloseModal={handleCloseModal} ideaId={ideaId}/>
+                <ModalForm handleCloseModal={handleCloseModal} ideaId={ideaId} setIsUploaded={setIsUploaded}/>
             </Modal>
         </>
     )
