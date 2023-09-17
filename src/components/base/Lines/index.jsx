55cd36a273f487f662fd66a8f081e09ed5e21ab6
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-17 21:01:53 +0300

    implemented map connections

---
+++ src/components/base/Lines/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Lines = (props) => {
+    const level = props.level;
+    const DX = props.isLeftSide ? -40 : 40;
+    const dx = props.x - props.px;
+    const dy = props.y - props.py;
+    const d = `M ${props.px} ${props.py} c ${DX} 0, ${dx-DX} ${dy}, ${dx} ${dy}`;
+    // x1={props.x} y1={props.y} x2={props.px} y2={props.py}
+    return (
+        <>{level === 0 ? (
+            <></>
+        ) : (
+            <g>
+                <path d={d} fill='none' stroke="#1e1e1e" />
+            </g>
+        )}</>
+    );
+};
+
+export default Lines;
