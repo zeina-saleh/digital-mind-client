60e896c6ff4a620a6739a7df8a4db9ffc9e1aaeb
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-20 20:50:32 +0300

    fixed input value bug

--- src/components/UI/Input/index.jsx
+++ src/components/UI/Input/index.jsx
@@ -6,7 +6,7 @@ const Input = ({onChange, label, placeholder = "", type = "text", value=""}) => 
     <div className='flex flex-col items-start w-full'>
         <div className={`wrapper2 flex flex-col items-start gap-1 w-full`}>
         <label className='px-2 font-medium' htmlFor={label.toLowerCase()}>{label}</label>
-            <input type={type} name={label.toLowerCase()} value={value} id={label.toLowerCase()} placeholder={placeholder} className={`input2 flex p-1 px-2 w-full`} onChange={(e) => onChange( e.target.value )}/></div>
+            <input type={type} name={label.toLowerCase()} defaultValue={value} id={label.toLowerCase()} placeholder={placeholder} className={`input2 flex p-1 px-2 w-full`} onChange={(e) => onChange( e.target.value )}/></div>
     </div>
   )
 }
