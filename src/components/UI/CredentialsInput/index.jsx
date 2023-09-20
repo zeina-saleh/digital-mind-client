2a755c24e7a586650a0de79d6aff25339b2ff273
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-20 18:06:14 +0300

    created credentials input

---
+++ src/components/UI/CredentialsInput/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CredentialsInput = ({ onChange, label, placeholder = "", type = "text" }) => {
+    return (
+        <div className='flex flex-col items-start w-full'>
+            <div className={`wrapper flex flex-col items-start gap-1 w-full`}>
+                <label className='px-2 font-medium' htmlFor={label.toLowerCase()}>{label}</label>
+                <input type={type} name={label.toLowerCase()} id={label.toLowerCase()} placeholder={placeholder} className={`input flex p-1 px-2 w-full`} onChange={(e) => onChange(e.target.value)} /></div>
+        </div>
+    )
+}
+
+export default CredentialsInput
