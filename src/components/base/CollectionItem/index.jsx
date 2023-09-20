815d602477aee64f2a168b3bccc747bdf4bf0288
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-20 15:12:22 +0300

    made idea card clickable

--- src/components/base/CollectionItem/index.jsx
+++ src/components/base/CollectionItem/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp, faSquareMinus, faPen, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
@@ -10,6 +10,7 @@ import logo from '../../../assets/logo.svg'
 const CollectionItem = ({ collection, setIdeaFunc, handleOpenModal, setCollectionId,
     handleOpenConsentModal, setIdeaId, editMode }) => {
 
+    const navigation = useNavigate();
     const [showIdeas, setShowIdeas] = useState(false)
     const [chevron, setChevron] = useState(faChevronDown)
 
@@ -24,7 +25,8 @@ const CollectionItem = ({ collection, setIdeaFunc, handleOpenModal, setCollectio
         handleOpenModal()
     }
 
-    const handleDeleteIdea = (ideaId) => {
+    const handleDeleteIdea = (event, ideaId) => {
+        event.stopPropagation()
         setIdeaFunc(true);
         setIdeaId(ideaId);
         handleOpenConsentModal()
@@ -44,6 +46,16 @@ const CollectionItem = ({ collection, setIdeaFunc, handleOpenModal, setCollectio
         handleOpenModal()
     }
 
+    const handleEditIdea = (ideaId) => {
+        setIdeaFunc(true);
+        setCollectionId(collection.id);
+        handleOpenModal()
+    }
+
+    const handleNavigation = (ideaId) => {
+        navigation(`/home/collections/idea/${ideaId}`)
+    }
+
     return (
         <div className='flex flex-col'>
             <div className='collection-item flex items-center justify-between p-2 cursor-pointer' onClick={onToggle}>
@@ -57,14 +69,18 @@ const CollectionItem = ({ collection, setIdeaFunc, handleOpenModal, setCollectio
                 <FontAwesomeIcon icon={chevron} style={{ color: "#1e1e1e", }} className='cursor-pointer' onClick={onToggle} />
             </div>
             {(collection.ideas.length !== 0 && showIdeas) ? (
-                <div className='flex flex-wrap px-10 py-4 gap-5 mt-4'>
+                <div className='flex flex-wrap ml-4 px-10 py-4 gap-5 mt-4'>
                     {collection.ideas.map(idea => (
                         <div key={idea.id} className='flex flex-col'>
-                            <div className='idea-item flex flex-col items-center w-64 h-60 px-2 pt-2'>
-                                <FontAwesomeIcon icon={faSquareMinus} style={{ color: "#1ED690", }} onClick={() => handleDeleteIdea(idea.id)} className='self-end cursor-pointer h-7 w-7' />
+                            <div className='idea-item flex flex-col items-center w-64 h-60 px-2 pt-2 cursor-pointer' onClick={() => handleNavigation(idea.id)}>
+                                <FontAwesomeIcon icon={faSquareMinus} style={{ color: "#1ED690", }} onClick={(event) => handleDeleteIdea(event, idea.id)} className='self-end cursor-pointer h-7 w-7' />
                                 <img src={logo} alt="" className='w-40 h-40' />
                             </div>
-                            <Link to={`/home/collections/idea/${idea.id}`}><div className='w-fit p-2 font-normal hover:text-[#1ED690]'>{idea.title}</div></Link>
+                            <div className='flex gap-3 items-center justify-between'>
+                            <div className='w-fit p-2 font-normal'>{idea.title}</div>
+                            <FontAwesomeIcon icon={faPen} className={`${editMode ? "" : 'hidden'} text-[#6b6b6b]  cursor-pointer hover:text-[#1ed690]`} onClick={() => handleEditIdea(idea.id)} />
+                            </div>
+
                         </div>
                     ))}
                     <div className='idea-item flex justify-center items-center gap-2 w-64 h-60 px-2 pt-2'>
