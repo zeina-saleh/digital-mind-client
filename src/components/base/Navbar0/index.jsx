6c9814abd56796b13312ad9b42bb0c728abc3623
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-20 18:34:19 +0300

    rendered home link when logged in instead of login btn

--- src/components/base/Navbar0/index.jsx
+++ src/components/base/Navbar0/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { Link } from 'react-scroll'
-import { useState } from 'react';
 import logo from '../../../assets/logo.svg'
-import constellation from '../../../assets/constellation.svg'
 import Button from '../../UI/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-const Navbar0 = () => {
+
+const Navbar0 = ({isLogged}) => {
 
     const navigation = useNavigate();
+
+    const home = async () => {
+            navigation('/home')
+    }
 
     const login = async () => {
             navigation('/login')
     }
+    
     return (
         <>
             <div className='flex justify-between items-center h-24 w-full mx-auto px-8 mb-5 fixed'>
@@ -23,7 +27,8 @@ const Navbar0 = () => {
                     <Link to='contact' spy={true} smooth={true} offset={0} duration={500} className='nav-link p-4 cursor-pointer'>Contact Us</Link>
                 </div>
                 <div className='flex items-center'>
-                    <Button text={"Login"} onClick={login} classname={'nav-link text-xl w-24 h-10'} />
+                    { isLogged? <div className='nav-link p-4 text-3xl font-medium cursor-pointer' onClick={home}>Home</div>
+                    : <Button text={"Login"} onClick={login} classname={'nav-link text-xl w-24 h-10'} /> }
                 </div>
             </div>
         </>
