7ad57f29c09d4935c94a38a7f45e4414eaa10ab5
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-21 17:56:09 +0300

    implemented schedule fetch function

--- src/pages/Planner/index.jsx
+++ src/pages/Planner/index.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { sendRequest } from '../../config/request'
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
+// import timeGridPlugin from '@fullcalendar/timegrid';
 import './style.css'
 
 const Planner = () => {
+
+  const [events, setEvents] = useState([]);
+
+  const fetchPlans = async () => {
+    try {
+      const response = await sendRequest({ route: "/getDate", body: "" });
+      console.log(response);
+      const schedule = response.schedule;
+      setEvents(schedule);
+      console.log(events)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlans();
+  }, []);
+
   return (
     <div className='flex justify-center w-full h-screen'>
       <div className='w-10/12'>
         <FullCalendar
-          plugins={[timeGridPlugin]}
-          initialView="timeGrid"
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
           weekends={true}
-          events={''}
+          events={events}
           eventBackgroundColor='#78FFCC'
         />
       </div>
