a7f1246b485eeb64fc11d0429ad7a3e4c7eb8799
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-27 10:15:47 +0300

    implemented dashboard numbers

--- src/components/admin_panel/Home/index.jsx
+++ src/components/admin_panel/Home/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSwatchbook, faComments, faFolderTree } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { nodeRequest } from '../../../config/nodeRequest';
 
 const Home = () => {
+
+    const [numbers, setNumbers] = useState([])
+
+    const fetchNumbers = async () => {
+        try {
+          const response = await nodeRequest({ route: "http://127.0.0.1:8000/stats", body: "" });
+          const nums = []
+          response.map((item) => {
+            nums.push(Object.values(item));
+          })
+          setNumbers(nums)
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      useEffect(() => {
+        fetchNumbers();
+      }, []);
+
     return (
         <main className='main-container ml-5'>
             <div className='main-title mb-5'>
@@ -15,7 +36,7 @@ const Home = () => {
                      <FontAwesomeIcon icon={faUser} className='w-7 h-7 text-[#F3F5F8] cursor-pointer' /></div>
                     <div className='flex flex-col'>
                         <p className='text-[#b1b1b1]'>Users</p>
-                        <p className='text-[#383838] font-bold'>number</p>
+                        <p className='text-[#383838] font-bold'>{numbers[0]}</p>
                     </div>
                 </div>
                 <div className='card-inner flex items-center gap-4'>
@@ -23,7 +44,7 @@ const Home = () => {
                      <FontAwesomeIcon icon={faSwatchbook} className='w-7 h-7 text-[#F3F5F8] cursor-pointer' /></div>
                     <div className='flex flex-col'>
                         <p className='text-[#b1b1b1]'>Collections</p>
-                        <p className='text-[#383838] font-bold'>number</p>
+                        <p className='text-[#383838] font-bold'>{numbers[1]}</p>
                     </div>
                 </div>
                 <div className='card-inner flex items-center gap-4'>
@@ -31,7 +52,7 @@ const Home = () => {
                      <FontAwesomeIcon icon={faComments} className='w-7 h-7 text-[#F3F5F8] cursor-pointer' /></div>
                     <div className='flex flex-col'>
                         <p className='text-[#b1b1b1]'>Discussions</p>
-                        <p className='text-[#383838] font-bold'>number</p>
+                        <p className='text-[#383838] font-bold'>{numbers[2]}</p>
                     </div>
                 </div>
                 <div className='card-inner flex items-center gap-4'>
@@ -39,7 +60,7 @@ const Home = () => {
                      <FontAwesomeIcon icon={faFolderTree} className='w-7 h-7 text-[#F3F5F8] cursor-pointer' /></div>
                     <div className='flex flex-col'>
                         <p className='text-[#b1b1b1]'>Files</p>
-                        <p className='text-[#383838] font-bold'>number</p>
+                        <p className='text-[#383838] font-bold'>{numbers[3]}</p>
                     </div>
                 </div>
 
