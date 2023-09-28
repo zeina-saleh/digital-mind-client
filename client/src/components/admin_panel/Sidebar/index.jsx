ac051ced59bb032832108abc27f5118a9a7eb6ae
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-28 22:57:01 +0300

    added dashboard tab

--- client/src/components/admin_panel/Sidebar/index.jsx
+++ client/src/components/admin_panel/Sidebar/index.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSwatchbook, faMagnifyingGlass, faCodeBranch, faFolderTree, faComments } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom'
 import logo from '../../../assets/logo-white.svg'
 
@@ -18,31 +19,21 @@ const Sidebar = () => {
             </div>
 
             <ul className='flex flex-col sidebar-list ml-10 gap-2 mt-16'>
+                <Link className='text-[#F3F5F8]' to="/admin"><li className='sidebar-list-item rounded-l-full focus:bg-[#F3F5F8]'>
+
+                    <FontAwesomeIcon icon={faGear} className='icon text-[#F3F5F8] cursor-pointer' /> Dashboard
+
+                </li></Link>
                 <Link className='text-[#F3F5F8]' to="/admin/users"><li className='sidebar-list-item rounded-l-full focus:bg-[#F3F5F8]'>
 
                     <FontAwesomeIcon icon={faUser} className='icon text-[#F3F5F8] cursor-pointer' /> Users
 
                 </li></Link>
-                {/* <Link className='text-[#F3F5F8]' to="/admin/collections"><li className='sidebar-list-item rounded-l-full'>
-
-                    <FontAwesomeIcon icon={faSwatchbook} className='icon text-[#F3F5F8] cursor-pointer' /> Collections
-
-                </li></Link>
-                <Link className='text-[#F3F5F8]' to="/admin/files"><li className='sidebar-list-item rounded-l-full'>
-
-                    <FontAwesomeIcon icon={faFolderTree} className='icon text-[#F3F5F8] cursor-pointer' /> Files
-
-                </li></Link> */}
                 <Link className='text-[#F3F5F8]' to="/admin/discussions"><li className='sidebar-list-item rounded-l-full'>
 
                     <FontAwesomeIcon icon={faComments} className='icon text-[#F3F5F8] cursor-pointer' /> Discussions
 
                 </li></Link>
-                {/* <li className='sidebar-list-item rounded-l-full'>
-                    <Link to="/admin">
-                        <FontAwesomeIcon icon={faComments} className='icon text-[#F3F5F8] cursor-pointer' /> Discussions
-                    </Link>
-                </li> */}
             </ul>
 
         </aside>
