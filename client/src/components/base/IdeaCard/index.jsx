273e42212750773b5072915dca91cc04d6a73478
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-30 13:40:56 +0300

    fixed pixelated map view

--- client/src/components/base/IdeaCard/index.jsx
+++ client/src/components/base/IdeaCard/index.jsx
@@ -8,11 +8,11 @@ import Modal from 'react-modal';
 
 const IdeaCard = ({ idea, setLikesCount }) => {
 
-    const [like, setLike] = useState({liked: idea.liked, count: idea.likes_count})
+    const [like, setLike] = useState({ liked: idea.liked, count: idea.likes_count })
     const [openResModal, setOpenResModal] = useState(false)
     const handleOpenResModal = () => setOpenResModal(true)
     const handleCloseResModal = () => setOpenResModal(false)
-    
+
 
     async function likeIdea() {
         try {
@@ -25,7 +25,7 @@ const IdeaCard = ({ idea, setLikesCount }) => {
 
     const handleLike = () => {
         likeIdea()
-        setLike({...like, liked:!like})
+        setLike({ ...like, liked: !like })
     }
 
     return (
@@ -34,28 +34,26 @@ const IdeaCard = ({ idea, setLikesCount }) => {
                 <div className='card flex items-center justify-center w-64 h-52 p-2' style={idea.path !== 'storage/images/logo.svg' ? { backgroundColor: '#fff' } : {}}>
                     <img className='card-img cursor-pointer' src={`http://localhost:8000/${idea.path}`} style={idea.path !== 'storage/images/logo.svg' ? { width: '100%', height: '100%', objectFit: 'cover' } : {}} onClick={handleOpenResModal}></img>
                 </div>
-                <div className='flex flex-col p-2 w-64'>
-                    <p className='font-semibold text-lg'>{idea.collection.user.name}</p>
-                        <p className='text-base'>{idea.title}</p>
+                <div className='flex flex-col px-3 w-64'>
+                    <p className='font-semibold text-lg'>{idea.title}</p>
+                    <span className='text-base'>by {idea.collection.user.name}</span>
                 </div>
             </div>
 
-            <Modal overlayClassName='overlay' isOpen={openResModal} onRequestClose={handleCloseResModal} className='modal flex flex-col py-10 px-20 bg-white h-5/6 w-2/3 items-center gap-4'>
-                <div className='flex justify-end w-full'>
+            <Modal overlayClassName='overlay' isOpen={openResModal} onRequestClose={handleCloseResModal} className='modal flex flex-col py-10 px-20 bg-white h-full w-2/3 items-center gap-4'>
+                <div className='flex justify-between w-full'>
+                    <div className='flex flex-col w-full gap-2'>
+                        <p className='text-2xl'>{idea.title}</p>
+                        <p className='text-base'>{idea.collection.user.name}</p>
+                    </div>
                     <FontAwesomeIcon icon={faXmark} className='text-[#1e1e1e] w-5 h-5 hover:text-[#1ae690] cursor-pointer' onClick={handleCloseResModal} />
                 </div>
-                <div className='flex flex-col w-full gap-2'>
-                    <p className='text-2xl'>{idea.title}</p>
-                    <p className='text-base'>{idea.collection.user.name}</p>
-                </div>
-                <div className='flex w-11/12 flex-1 justify-between h-2/3'>
-                    <img src={`http://localhost:8000/${idea.path}`} alt="img" className={ idea.path=='storage/images/logo.svg'? 'h-72 w-72' : `img-res w-full`} />
-                    <div className='flex flex-col h-1/2 justify-around items-center gap-5'>
-                    </div>
+                <div className='flex items-center w-5/6 h-full'>
+                    <img src={`http://localhost:8000/${idea.path}`} alt="map" className={idea.path == 'storage/images/logo.svg' ? 'h-72 w-72' : `img-res scale-125`} />
                 </div>
                 <div className='flex gap-3 self-start items-baseline'>
-                <FontAwesomeIcon icon={faSwatchbook} style={{ color: "#1e1e1e", }} className='w-6 h-6' />
-                <div>{idea.collection.title} Collection</div>
+                    <FontAwesomeIcon icon={faSwatchbook} style={{ color: "#1e1e1e", }} className='w-6 h-6' />
+                    <div>{idea.collection.title} Collection</div>
                 </div>
             </Modal>
         </>
