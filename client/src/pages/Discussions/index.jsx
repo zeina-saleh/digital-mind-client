2fc80662c9da768f0ce333344affa0219410cdf2
zeinaCodes <eye_likemoon@hotmail.com>
2023-10-01 00:45:28 +0300

    added discussions page empty state

--- client/src/pages/Discussions/index.jsx
+++ client/src/pages/Discussions/index.jsx
@@ -5,11 +5,13 @@ import './style.css'
 import DiscussionCard from '../../components/base/DiscussionCard'
 import { messaging } from '../../config/firebase';
 import { getToken } from 'firebase/messaging';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
 
 const Discussions = () => {
 
   const [discussions, setDiscussions] = useState([])
-  const [user, setUser] = useState({id:null, name:null})
+  const [user, setUser] = useState({ id: null, name: null })
   const [exit, setExit] = useState(0)
   const [editMode, setEditMode] = useState(false)
   const [tokens, setTokens] = useState(null)
@@ -20,11 +22,11 @@ const Discussions = () => {
       const token = await getToken(messaging, { vapidKey: 'BO6L1QNjM1ZshA5CvXqEC7XxfIdzGyBZfV6iTQzpL69f3uiT4oIVIbZgCXr5LruMlB76VR-zZscGcH7m_3PbVPM' })
       console.log(token)
       try {
-        const response = await sendRequest({ method: "POST", route: '/saveToken', body: {token: token} });
+        const response = await sendRequest({ method: "POST", route: '/saveToken', body: { token: token } });
         setTokens(response.tokens)
-    } catch (error) {
+      } catch (error) {
         console.log(error);
-    }
+      }
 
     } else if (permission === 'denied') {
       console.log('permission denied')
@@ -33,7 +35,7 @@ const Discussions = () => {
 
   useEffect(() => {
     requestPermission()
-  },[])
+  }, [])
 
   const fetchDiscussions = async () => {
     try {
@@ -60,14 +62,20 @@ const Discussions = () => {
         <div className='flex items-center justify-between w-10/12'>
           <div className='text-3xl page-title'>My Discussions</div>
         </div>
-
-        <div className='flex ml-56 w-full gap-5 flex-wrap'>
-          {discussions.map(discussion => (
-            <DiscussionCard key={discussion.id} discussion={discussion} user={user} setExit={setExit} setEditMode={setEditMode} tokens={tokens}/>
-          ))}
-        </div>
-
-        
+        {discussions.length === 0 ? (
+          <section className='flex flex-col gap-4 justify-center items-center empty-state w-full'>
+            <FontAwesomeIcon icon={faComments} className='h-52 w-52 text-[#1ae690]' />
+            <div className='flex flex-col gap-1 items-center'>
+              <div className='empty-div'>You have no discussions</div>
+            </div>
+          </section>
+        ) : (
+          <div className='flex ml-56 w-full gap-5 flex-wrap'>
+            {discussions.map(discussion => (
+              <DiscussionCard key={discussion.id} discussion={discussion} user={user} setExit={setExit} setEditMode={setEditMode} tokens={tokens} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
